2046a5f59b31f2744b3b6739fcbb33a9fa63b485
Zamarron <emilio.zamarrons@gmail.com>
2025-01-20 14:19:04 -0600

    first commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int daysUntilChristmas = (new DateTime(DateTime.Now.Year, 12, 25) - DateTime.Now).Days;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+Console.WriteLine($"there are {daysUntilChristmas} days until the next Christmas");
